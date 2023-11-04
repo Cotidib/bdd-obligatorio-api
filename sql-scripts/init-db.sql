e3d9decd173914b4740058424644f8b4fce0c50d
Cotidib <constanza.dibueno@gmail.com>
2023-11-04 17:57:55 -0300

    Comentario docker compose y agregar inserts de prueba en init-db

--- sql-scripts/init-db.sql
+++ sql-scripts/init-db.sql
@@ -3,3 +3,15 @@ CREATE TABLE obligatoriodb.Weather (
 	name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (wid)
 );
+
+INSERT INTO obligatoriodb.Weather (wid, name) VALUES
+	(1, 'Freezing'),
+	(2, 'Bracing'),
+	(3, 'Chilly'),
+	(4, 'Cool'),
+	(5, 'Mild'),
+	(6, 'Warm'),
+	(7, 'Balmy'),
+	(8, 'Hot'),
+	(9, 'Sweltering'),
+	(10, 'Scorching');
