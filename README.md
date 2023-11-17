23362ea4686995d8171b172ab725ff643efcef59
Cotidib <constanza.dibueno@gmail.com>
2023-11-17 19:55:24 -0300

    create tables

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bdd-obligatorio-api
+
+
+## Flujo Sig In (Persona no registrada)
+Formulario Registrarse > Formulario datos personales (Funcionario) > Dashboard (¿Carnet de Salud o Agenda?) 
+## Flujo Log in (Persona ya registrada)
+Formulario Login > Autenticado > Dashboard (¿Carnet de Salud o Agenda?) 
--- sql-scripts/init-db.sql
+++ sql-scripts/init-db.sql
@@ -16,14 +16,38 @@ INSERT INTO obligatoriodb.Weather (wid, name) VALUES
 	(9, 'Sweltering'),
 	(10, 'Scorching');
 
+CREATE TABLE obligatoriodb.Logins(
+	Logid VARCHAR(30) NOT NULL,
+	Pwd VARCHAR(100) NOT NULL,
+	PRIMARY KEY (Logid)
+);
+
 CREATE TABLE obligatoriodb.Funcionarios (
 	Ci INT NOT NULL,
-	Email VARCHAR(30) NOT NULL,
-	PRIMARY KEY (Ci)
+	Nombre VARCHAR(30), 
+	Apellido VARCHAR(30),
+	Email VARCHAR(60) NOT NULL,
+	Fch_Nacimiento DATE,
+	Direccion VARCHAR(60),
+	Telefono INT,
+	Logid INT NOT NULL,
+	PRIMARY KEY (Ci),
+	FOREIGN KEY (Logid) REFERENCES Logins(Logid)
+);
+
+CREATE TABLE obligatoriodb.Agenda (
+	Nro INT NOT NULL,
+	Ci INT NOT NULL,
+	Fch_Agenda DATE TIME NOT NULL,
+	PRIMARY KEY (Nro),
+	FOREIGN KEY (Ci) REFERENCES Funcionarios(Ci)
 );
 
 CREATE TABLE obligatoriodb.Carnet_Salud (
 	Ci INT NOT NULL,
+	Fch_Emision DATE NOT NULL,
+	Fch_Vencimiento DATE NOT NULL,
+	Comprobante VARCHAR(100),
 	FOREIGN KEY (Ci) REFERENCES Funcionarios(Ci)
 );
 
