8f3eca5315febc753c6a5d130c1ee0fcf65a28e4
MilagrosKucharski <milagros.kucharski@correo.ucu.edu.uy>
2023-11-20 12:25:05 -0300

    Otros controllers

--- bdd-obligatorio-api/Controllers/AgendaController.cs
+++ bdd-obligatorio-api/Controllers/AgendaController.cs
@@ -1,41 +1,33 @@
+using bdd_obligatorio_api.Models;
+using bdd_obligatorio_api.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using MySqlConnector;
 using System;
 
 namespace bdd_obligatorio_api.Controllers
 {
     [ApiController]
-    [Route("[/]")]
+    [Route("api/agendas")]
     public class AgendaController : ControllerBase
     {
-        private IConfiguration _config;
-        private MySqlConnection _conn;
+        private readonly IAgendaRepository _agendaRepository;
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public AgendaController(IConfiguration config, ILogger<WeatherForecastController> logger)
+        public AgendaController(IAgendaRepository agendaRepository)
         {
-            _logger = logger;
-            _config = config;
-            _conn = new MySqlConnection(config.GetConnectionString("Default"));
+            _agendaRepository = agendaRepository;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public async Task<IEnumerable<WeatherForecast>> Get()
+        [HttpPost]
+        public IActionResult AddAgenda([FromBody] Agenda agenda)
         {
-            await _conn.OpenAsync();
-            // Retrieve all rows
-            // REFERENCIA LIBRERIA MYSQL CONNECTOR https://mysqlconnector.net/tutorials/connect-to-mysql/
-            using var command = new MySqlCommand("SELECT name FROM Weather", _conn);
-            using var reader = await command.ExecuteReaderAsync();
-
-            List<WeatherForecast> weathers = new List<WeatherForecast> { };
-            while (await reader.ReadAsync())
-                {
-                weathers.Add(new WeatherForecast(reader.GetString(0)));
-                }
-
-            return weathers;
+            try
+            {
+                _agendaRepository.AddAgenda(agenda);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error al agregar la agenda: {ex.Message}");
+            }
         }
     }
 }
